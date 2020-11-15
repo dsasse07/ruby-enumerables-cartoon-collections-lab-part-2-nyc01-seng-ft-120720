e2c19c5e905466b57cab4b829894cee6cb7bf4a8
Daniel Sasse <dsasse07@gmail.com>
2020-11-15 18:59:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -33,7 +33,5 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
   
   planeteer_calls.find do |element|
-    if valid_calls.any? {|element|}
-    end
-  end
+    valid_calls.any?
 end
